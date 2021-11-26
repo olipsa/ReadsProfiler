8236c17557591ccb95f55d9ecc8c68685db96c5a
ondina <ondina_patricia@yahoo.com>
2021-11-26 16:23:41 +0200

    Added database to git

---
+++ cmake-build-debug/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/Client/Client.cpp.o"
+  "CMakeFiles/client.dir/Client/ClientNetwork.cpp.o"
+  "CMakeFiles/client.dir/Client/main.cpp.o"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/Server/Commands/Command.cpp.o"
+  "CMakeFiles/server.dir/Server/Commands/Login.cpp.o"
+  "CMakeFiles/server.dir/Server/Commands/Register.cpp.o"
+  "CMakeFiles/server.dir/Server/Commands/Search.cpp.o"
+  "CMakeFiles/server.dir/Server/Database.cpp.o"
+  "CMakeFiles/server.dir/Server/Server.cpp.o"
+  "CMakeFiles/server.dir/Server/ServerNetwork.cpp.o"
+  "CMakeFiles/server.dir/Server/main.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
